6e3cec96b15ead69b870e94124425b1f4a06b15c
전상욱 <80368246+UGKONG@users.noreply.github.com>
2024-04-18 19:29:54 +0900

    UPDATE

--- 계산기/build/index.js
+++ 계산기/build/index.js
@@ -1,42 +1,132 @@
-var _a, _b, _c;
+var _a, _b, _c, _d, _e, _f;
 // 계산결과
-var result = 235325;
-var unit = null;
+var prev = 0;
+var result = 0;
+var operator = null;
+var calcHistory = [];
+var OPERATOR = {
+    곱셈: "*",
+    나눗셈: "/",
+    덧셈: "+",
+    뺄셈: "-",
+};
 // 요소
 var el = {
     결과: document.querySelector("#result > span"),
-    곱셈: document.querySelector("#곱셈"),
-    뺄셈: document.querySelector("#뺄셈"),
-    덧셈: document.querySelector("#덧셈"),
+    숫자: document.querySelectorAll(".num"),
+    연산자: document.querySelectorAll("#곱셈,#뺄셈,#덧셈,#나눗셈"),
     계산: document.querySelector("#계산"),
-    나눗셈: document.querySelector("#나눗셈"),
     지우기: document.querySelector("#지우기"),
     초기화: document.querySelector("#초기화"),
     퍼센트: document.querySelector("#퍼센트"),
+    히스토리: document.querySelector("#히스토리"),
+};
+// 현재 DATETIME 조회
+var getCurrentTimeStamp = function () {
+    var date = new Date();
+    var Y = String(date === null || date === void 0 ? void 0 : date.getFullYear());
+    var M = (date === null || date === void 0 ? void 0 : date.getMonth()) + 1;
+    var D = date === null || date === void 0 ? void 0 : date.getDate();
+    var h = date === null || date === void 0 ? void 0 : date.getHours();
+    var m = date === null || date === void 0 ? void 0 : date.getMinutes();
+    var s = date === null || date === void 0 ? void 0 : date.getSeconds();
+    M = String(M < 10 ? "0" + M : M);
+    D = String(D < 10 ? "0" + D : D);
+    h = String(h < 10 ? "0" + h : h);
+    m = String(m < 10 ? "0" + m : m);
+    s = String(s < 10 ? "0" + s : s);
+    var result = "".concat(Y, "-").concat(M, "-").concat(D, " ").concat(h, ":").concat(m, ":").concat(s);
+    return result;
+};
+// 히스토리 랜더링
+var historyRender = function () {
+    el.히스토리.innerHTML = "";
+    if (!(calcHistory === null || calcHistory === void 0 ? void 0 : calcHistory.length)) {
+        return (el.히스토리.innerHTML = "<li>히스토리가 없습니다.</li>");
+    }
+    calcHistory === null || calcHistory === void 0 ? void 0 : calcHistory.forEach(function (item) {
+        var _a;
+        var li = document.createElement("li");
+        li.innerHTML = "\n      <small>[".concat(item === null || item === void 0 ? void 0 : item.date, "]</small>\n      <p><b>").concat(item === null || item === void 0 ? void 0 : item.calcString, " = ").concat(item === null || item === void 0 ? void 0 : item.calcResult, "</b></p>\n    ");
+        (_a = el === null || el === void 0 ? void 0 : el.히스토리) === null || _a === void 0 ? void 0 : _a.appendChild(li);
+    });
+};
+// 히스토리 추가
+var addHistory = function (data) {
+    calcHistory.push(data);
+    if (calcHistory.length > 10)
+        calcHistory.shift();
+    historyRender();
 };
 // 랜더링
 var render = function () {
+    var _a, _b;
+    // 결과 반영
     el.결과.innerText = String(result);
+    // 스타일
+    (_a = el === null || el === void 0 ? void 0 : el.연산자) === null || _a === void 0 ? void 0 : _a.forEach(function (x) { var _a; return (_a = x === null || x === void 0 ? void 0 : x.classList) === null || _a === void 0 ? void 0 : _a.remove("active"); });
+    if (operator) {
+        var operatorEl = document.querySelector("#" + operator);
+        (_b = operatorEl === null || operatorEl === void 0 ? void 0 : operatorEl.classList) === null || _b === void 0 ? void 0 : _b.add("active");
+    }
 };
-// 지우기 함수
+// 하나 지우기
 var oneDelete = function () {
     var _a;
     result = Number((_a = String(result)) === null || _a === void 0 ? void 0 : _a.slice(0, -1));
     render();
 };
+// 전체 지우기
 var allDelete = function () {
+    prev = 0;
     result = 0;
+    operator = null;
     render();
 };
-// 연산 함수
+// 퍼센트
 var percent = function () {
     result = result / 100;
     render();
 };
-var division = function () { };
+// 계산
+var calculation = function (isNotRender) {
+    if (isNotRender === void 0) { isNotRender = false; }
+    if (!operator)
+        return;
+    var calcString = "".concat(prev, " ").concat(OPERATOR[operator], " ").concat(result);
+    var calc = window === null || window === void 0 ? void 0 : window.eval(calcString);
+    addHistory({ calcString: calcString, calcResult: calc, date: getCurrentTimeStamp() });
+    result = calc;
+    operator = null;
+    prev = 0;
+    if (isNotRender)
+        return;
+    render();
+};
+// 연산자 누르기
+var operatorClick = function (e) {
+    var _a;
+    var id = (_a = e === null || e === void 0 ? void 0 : e.target) === null || _a === void 0 ? void 0 : _a.id;
+    if (operator)
+        calculation(true);
+    prev = result;
+    operator = id;
+    result = 0;
+    render();
+};
+// 숫자 누르기
+var numberClick = function (e) {
+    var id = (e === null || e === void 0 ? void 0 : e.target).id;
+    result = Number(String(result) + id);
+    render();
+};
 // 이벤트 바인딩
 (_a = el === null || el === void 0 ? void 0 : el.지우기) === null || _a === void 0 ? void 0 : _a.addEventListener("click", oneDelete);
 (_b = el === null || el === void 0 ? void 0 : el.초기화) === null || _b === void 0 ? void 0 : _b.addEventListener("click", allDelete);
 (_c = el === null || el === void 0 ? void 0 : el.퍼센트) === null || _c === void 0 ? void 0 : _c.addEventListener("click", percent);
+(_d = el === null || el === void 0 ? void 0 : el.계산) === null || _d === void 0 ? void 0 : _d.addEventListener("click", function () { return calculation(); });
+(_e = el === null || el === void 0 ? void 0 : el.숫자) === null || _e === void 0 ? void 0 : _e.forEach(function (x) { return x === null || x === void 0 ? void 0 : x.addEventListener("click", numberClick); });
+(_f = el === null || el === void 0 ? void 0 : el.연산자) === null || _f === void 0 ? void 0 : _f.forEach(function (x) { return x === null || x === void 0 ? void 0 : x.addEventListener("click", operatorClick); });
 // 호출
 render();
+historyRender();
--- 계산기/index.ts
+++ 계산기/index.ts
@@ -1,46 +1,147 @@
+// 타입 정의
+type Operator = "곱셈" | "나눗셈" | "덧셈" | "뺄셈";
+type CalcHistory = { calcString: string; calcResult: number; date: string };
+
 // 계산결과
-let result = 235325;
-let unit: null | "곱셈" | "뺄셈" | "덧셈" | "나눗셈" | "퍼센트" = null;
+let prev = 0;
+let result = 0;
+let operator: null | Operator = null;
+let calcHistory: CalcHistory[] = [];
+
+const OPERATOR = {
+  곱셈: "*",
+  나눗셈: "/",
+  덧셈: "+",
+  뺄셈: "-",
+} as const;
 
 // 요소
 const el = {
   결과: document.querySelector("#result > span") as HTMLSpanElement,
-  곱셈: document.querySelector("#곱셈") as HTMLDivElement,
-  뺄셈: document.querySelector("#뺄셈") as HTMLDivElement,
-  덧셈: document.querySelector("#덧셈") as HTMLDivElement,
+  숫자: document.querySelectorAll(".num") as NodeListOf<HTMLDivElement>,
+  연산자: document.querySelectorAll(
+    "#곱셈,#뺄셈,#덧셈,#나눗셈"
+  ) as NodeListOf<HTMLDivElement>,
   계산: document.querySelector("#계산") as HTMLDivElement,
-  나눗셈: document.querySelector("#나눗셈") as HTMLDivElement,
   지우기: document.querySelector("#지우기") as HTMLDivElement,
   초기화: document.querySelector("#초기화") as HTMLDivElement,
   퍼센트: document.querySelector("#퍼센트") as HTMLDivElement,
+  히스토리: document.querySelector("#히스토리") as HTMLUListElement,
+};
+
+// 현재 DATETIME 조회
+const getCurrentTimeStamp = () => {
+  let date = new Date();
+  let Y: string | number = String(date?.getFullYear());
+  let M: string | number = date?.getMonth() + 1;
+  let D: string | number = date?.getDate();
+  let h: string | number = date?.getHours();
+  let m: string | number = date?.getMinutes();
+  let s: string | number = date?.getSeconds();
+  M = String(M < 10 ? "0" + M : M);
+  D = String(D < 10 ? "0" + D : D);
+  h = String(h < 10 ? "0" + h : h);
+  m = String(m < 10 ? "0" + m : m);
+  s = String(s < 10 ? "0" + s : s);
+  let result = `${Y}-${M}-${D} ${h}:${m}:${s}`;
+  return result;
+};
+
+// 히스토리 랜더링
+const historyRender = () => {
+  el.히스토리.innerHTML = "";
+
+  if (!calcHistory?.length) {
+    return (el.히스토리.innerHTML = "<li>히스토리가 없습니다.</li>");
+  }
+
+  calcHistory?.forEach((item) => {
+    let li = document.createElement("li");
+    li.innerHTML = `
+      <small>[${item?.date}]</small>
+      <p><b>${item?.calcString} = ${item?.calcResult}</b></p>
+    `;
+    el?.히스토리?.appendChild(li);
+  });
+};
+
+// 히스토리 추가
+const addHistory = (data: CalcHistory) => {
+  calcHistory.push(data);
+  if (calcHistory.length > 10) calcHistory.shift();
+  historyRender();
 };
 
 // 랜더링
 const render = () => {
+  // 결과 반영
   el.결과.innerText = String(result);
-};
 
-// 지우기 함수
+  // 스타일
+  el?.연산자?.forEach((x) => x?.classList?.remove("active"));
+  if (operator) {
+    let operatorEl = document.querySelector("#" + operator) as HTMLDivElement;
+    operatorEl?.classList?.add("active");
+  }
+};
+// 하나 지우기
 const oneDelete = () => {
   result = Number(String(result)?.slice(0, -1));
   render();
 };
+// 전체 지우기
 const allDelete = () => {
+  prev = 0;
   result = 0;
+  operator = null;
   render();
 };
-
-// 연산 함수
+// 퍼센트
 const percent = () => {
   result = result / 100;
   render();
 };
-const division = () => {};
+// 계산
+const calculation = (isNotRender: boolean = false) => {
+  if (!operator) return;
+  let calcString = `${prev} ${OPERATOR[operator]} ${result}`;
+  let calc = window?.eval(calcString);
+
+  addHistory({ calcString, calcResult: calc, date: getCurrentTimeStamp() });
+
+  result = calc;
+  operator = null;
+  prev = 0;
+
+  if (isNotRender) return;
+  render();
+};
+// 연산자 누르기
+const operatorClick = (e: MouseEvent) => {
+  const id = (e?.target as HTMLDivElement)?.id as Operator;
+
+  if (operator) calculation(true);
+
+  prev = result;
+  operator = id;
+  result = 0;
+  render();
+};
+// 숫자 누르기
+const numberClick = (e: MouseEvent) => {
+  const { id } = e?.target as HTMLDivElement;
+  result = Number(String(result) + id);
+  render();
+};
 
 // 이벤트 바인딩
 el?.지우기?.addEventListener("click", oneDelete);
 el?.초기화?.addEventListener("click", allDelete);
 el?.퍼센트?.addEventListener("click", percent);
+el?.계산?.addEventListener("click", () => calculation());
+el?.숫자?.forEach((x) => x?.addEventListener("click", numberClick));
+el?.연산자?.forEach((x) => x?.addEventListener("click", operatorClick));
 
 // 호출
 render();
+historyRender();
